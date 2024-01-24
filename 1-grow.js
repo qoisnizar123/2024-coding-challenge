70b83f0407c03140cf068e4563fe48967c88872c
qoisnizarmahendra <nizarmahendra919@gmail.com>
2024-01-24 11:41:02 +0700

    day 1

---
+++ 1-grow.js
@@ -0,0 +1,19 @@
+// coding challenge 2024
+// day 1/366
+
+function grow(x){
+    let result = x[0];
+    for (let i = 1; i < x.length; i++){
+        result *= x[i];
+    }
+    return result;
+}
+
+console.log(grow([2, 2, 2, 2, 2, 2]));
+ 
+// function grow(x) {
+//     const result = x.reduce((acc, curr) => acc * curr, 1);
+//     return result;
+// }
+
+// console.log(grow([2, 2, 2, 2, 2, 2]))
